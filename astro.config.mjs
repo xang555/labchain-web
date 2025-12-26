c177143716dc6d7a04298281da13443fd9de81ab
xang Ultimate <xang.ultimate@gmail.com>
2025-12-26 21:08:33 +0700

    #init

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  site: 'https://labchain.la',
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto'
+  }
+});
